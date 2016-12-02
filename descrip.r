2244dd9530433a68f3a0d5e4d03a599867054d2a
Dani Socas <Dani Socas>
2016-12-02 19:29:13 +0000

    separacion de files,funcion load con algunos cambio y edad exacta

---
+++ descrip.r
@@ -0,0 +1,69 @@
+"Extradition of knowledge and Machine Learning
+Samuel Arleo & Daniela Socas
+Last modified 12/02/2016
+Descriptive functions to start the project and pre-processing"
+
+#------------------ LOAD DATA -------------------------
+
+# Table files names
+tables <- c("account", "card_test", "card_train", "client", "disp", "district", "loan_test", 
+            "loan_train", "trans_test", "trans_train")
+
+# Change here the path of data
+#path <- "~/Mineria/Data/"
+path <- "C:/Users/Dusady/Documents/Dan/UP/ML/Loan-Prediction-Data-Analytics/Data"
+
+loadData( path ,na_values)
+
+# Values of empty fields in .csv files
+na_values <- c(""," ","NA","?")
+
+# Putting al tables in a list for passing them to checkNA
+dframes <- list.files(path)
+
+# Checking number of NA field for each table
+checkNa(dframes)
+
+#------------------ CLEANING AGE/GENDER -------------------------
+
+# Max date on client$birth_number, card$issued to avoid considering dates like 11-01-01 as 2011-01-01 but 1911-01-01
+# because data is older than 
+refDate <- max(client.csv$birth_number,card_train.csv$issued,card_test.csv$issued,account.csv$date,
+               trans_train.csv$date,trans_test.csv$date,loan_test.csv$date,loan_train.csv$date)
+
+
+# Adding new columns of gender and age to client table
+client.csv$gender<-unlist(lapply(client.csv$birth_number,getGender))
+client.csv$age<-unlist(lapply(client.csv$birth_number,getAntiquity,refDate))
+
+#Exact age 
+client.csv$age<-round(client.csv$age)
+
+# Joining disp (table that maps clients IDs to accounts IDs) and client tables by client_id
+m1 = mergeTables(disp.csv[,c("client_id","account_id")], client.csv[,c("client_id","gender","age")],"client_id")
+
+# Joining m1 with loan data by account_id
+m2 = mergeTables(m1,loan_train.csv[,c("account_id","status")],"account_id")
+
+#------------------ PLOTS -------------------------
+
+# Age vs Status
+boxplot(age~status,data=m2)
+
+# By gender
+boxplot(age~status,data=m2[m2$gender == "0",])
+boxplot(age~status,data=m2[m2$gender == "1",])
+
+# Contingency table for gender vs status
+c1 <- table(m2$gender,m2$status)
+
+# Barplot gender vs status
+barplot(c1,main = "Gender-Status frequencies")
+
+#------------------ OUTLIERS -------------------------
+
+# Esto es para ver los outliers de las columnas numericas importantes
+# Probar al final el modelo entrenado con y sin outliers
+#boxplot(district[,4:16])
+#boxplot(loan_train$amount)
+
---
+++ descrip_fun.r
@@ -0,0 +1,91 @@
+"Extradition of knowledge and Machine Learning
+Samuel Arleo & Daniela Socas
+Last modified 12/02/2016
+Descriptive functions to start the project and pre-processing"
+
+#------------------ LOADING DATA -------------------------
+
+# Disabling scientific notation
+options(scipen=999)
+
+# Loading data
+loadData <- function(path, empty_values)
+{
+	files <- list.files(path)
+	for (i in files){
+		a <- read.csv2(i, header = TRUE, na.strings=na_values)
+		assign(i, a)
+	}
+	remove(a)
+}
+
+
+#------------------ NA VALUES -------------------------
+
+# Counts the number of NA fields in a column 
+countNa <- function(df)
+  {
+    cat(df)
+    cat(colnames(df))
+    for(col in colnames(df)){
+      cat(col," ",sum(is.na(df[[col]])),"\n")
+      
+    }
+}
+
+# Prints number of NAs for each column
+checkNa <- function(frames)
+{
+	for(f in frames){
+		countNa(f)
+		cat("  ###############################\n")
+	}
+}
+
+#------------------ DATES -------------------------
+
+# Takes out the +50 months if date is the birthday of a female client  
+formatDate <- function(date)
+{
+	if (substr(toString(date),3,4) > "12"){
+		return(date-5000)
+	} else {
+		return(date)
+	}
+}
+
+# Determines the gender of a client using the birthdate (female dates have month +50)
+getGender <- function(date)
+{
+	if (substr(toString(date),3,4) <= "12"){
+			return("0")
+	} else {
+			return("1")
+	}
+}
+
+# Difference in time (weeks) between given date and reference date
+getAntiquity <- function(date, refDate)
+{
+	return(difftime(strptime(toString(refDate), format = "%y%m%d"),
+	strptime(paste("19",toString(formatDate(date)),sep=""), format = "%Y%m%d"),units="weeks")/52.25)
+}
+
+mergeTables <- function(df1,df2,colName)
+{
+	merge(df1,df2,by=colName)
+}
+
+#------------------ DUDAS -------------------------
+
+"
+- Los prestamos se toman como ya finalizados?
+- En el enunciado dice que hay varios status A,B,C,D pero en los archivos aparece 1 y -1
+- Hay datos de testing para las relaciones card, loan y transaction. Hay que hacer predicciones de algo mas ademas
+de si un prestamo terminara de forma exitosa o no?
+- sanction interest if negative balance?
+- Quitar ceros de account en transfers? es noise? afecta en algo?
+- interest credited -- interest that a savings institution automatically deposits to a savings account. 
+- Falta la relacion Permanent order (esta en el enunciado pero no hay ningun archivo con ese nombre)
+- Usar contingency tables para comparar nominal-nominal y boxplot para numerical-nominal
+"
